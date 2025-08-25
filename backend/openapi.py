cba6e4a964520eb13b28e3f36123f8a8b12085ce
Mike Pryakhin <mikhail.pryakhin@intel.com>
2025-08-25 11:15:19 +0100

    Create a CI skeleton (#72)

---
+++ backend/openapi.py
@@ -0,0 +1,18 @@
+import json
+import pathlib
+import argparse
+from app.main import app
+from fastapi.openapi.utils import get_openapi
+
+parser = argparse.ArgumentParser(description="Generate OpenAPI schema")
+parser.add_argument("output_path", type=pathlib.Path, help="The path where the openapi-spec.json file will be saved.")
+args = parser.parse_args()
+
+output_file = pathlib.Path(args.output_path) / "openapi-spec.json"
+output_file.parent.mkdir(parents=True, exist_ok=True)
+
+with open(output_file, "w") as file:
+    open_api = app.openapi()
+    json.dump(obj=open_api, fp=file, indent=2)
+
+print(f"OpenAPI schema generated at {output_file}")
