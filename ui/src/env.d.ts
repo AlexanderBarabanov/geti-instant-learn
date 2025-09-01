deb47ddbb524a3f2f763e7d4d4e6b36863501631
pplaskie <97158765+pplaskie@users.noreply.github.com>
2025-09-01 16:41:52 +0200

    Eslint and typescript configuration (#70)
    Co-authored-by: Dawid Wesołowski <dawid.wesolowski@intel.com>

--- ui/src/env.d.ts
+++ ui/src/env.d.ts
@@ -5,12 +5,12 @@
 
 /// <reference types="@rsbuild/core/types" />
 
-/**
- * Imports the SVG file as a React component.
- * @requires [@rsbuild/plugin-svgr](https://npmjs.com/package/@rsbuild/plugin-svgr)
- */
+declare module '*.svg' {
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+}
 declare module '*.svg?react' {
-  import type React from 'react';
   const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
 }
