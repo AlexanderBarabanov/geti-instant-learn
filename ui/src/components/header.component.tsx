8b67fe3a753956d4293767a1710f6630337a955d
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-09-03 12:51:24 +0200

    chore: Configure prettier

--- ui/src/components/header.component.tsx
+++ ui/src/components/header.component.tsx
@@ -3,14 +3,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Header as SpectrumHeader, View, Flex } from '@geti/ui';
+import { Flex, Header as SpectrumHeader, View } from '@geti/ui';
 
 export const Header = () => {
-  return (
-    <View backgroundColor={'gray-300'} height="size-800">
-      <Flex height="100%" alignItems={'center'} marginX="1rem" gap="size-200">
-        <SpectrumHeader>Geti Prompt</SpectrumHeader>
-      </Flex>
-    </View>
-  );
+    return (
+        <View backgroundColor={'gray-300'} height='size-800'>
+            <Flex height='100%' alignItems={'center'} marginX='1rem' gap='size-200'>
+                <SpectrumHeader>Geti Prompt</SpectrumHeader>
+            </Flex>
+        </View>
+    );
 };
