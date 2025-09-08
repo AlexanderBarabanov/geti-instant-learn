c05f753a2bfe144b582bc380402341b8dc1572a9
Dawid Wesołowski <dawid.wesolowski@intel.com>
2025-09-08 15:25:16 +0200

    feat: add icons

--- ui/src/assets/icons/index.tsx
+++ ui/src/assets/icons/index.tsx
@@ -4,3 +4,9 @@
  */
 
 export { ReactComponent as NoMedia } from './no-media.svg';
+export { ReactComponent as InputOutput } from './input-output.svg';
+export { ReactComponent as GenICam } from './genicam.svg';
+export { ReactComponent as ImagesFolder } from './images-folder.svg';
+export { ReactComponent as IPCamera } from './ip-camera.svg';
+export { ReactComponent as VideoFile } from './video-file.svg';
+export { ReactComponent as WebCam } from './webcam.svg';
