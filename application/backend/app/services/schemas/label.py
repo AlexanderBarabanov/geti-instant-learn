cdf3d8b3ab2bc8da1a2ab64b5dd4fbe4ac081791
Michał Adamczyk <michal1.adamczyk@intel.com>
2025-10-24 10:24:15 +0200

    add label management (#251)

---
+++ application/backend/app/services/schemas/label.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+from pydantic import BaseModel, Field
+from pydantic_extra_types.color import Color
+
+from services.schemas.base import BaseIDPayload, BaseIDSchema, Pagination
+
+
+class LabelCreateSchema(BaseIDPayload):
+    name: str = Field("", min_length=1, max_length=50, description="Label name")
+    color: Color | None = Field(None, description="New hex color code, e.g. #RRGGBB or #RGB")
+
+
+class LabelSchema(BaseIDSchema):
+    name: str = Field("", min_length=1, max_length=50, description="Label name")
+    color: Color | None = Field(None, description="New hex color code, e.g. #RRGGBB or #RGB")
+
+
+class LabelsListSchema(BaseModel):
+    labels: list[LabelSchema]
+    pagination: Pagination
